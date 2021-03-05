016c42ddbf435fbea0a4c3d96cfa612477ee7d59
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-03-06 00:21:31 +0700

    Custom collor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#fefcbf", // For lighter primary color
+          DEFAULT: "#000000", // Normal primary color
+          dark: "#383838", // Used for hover, active, etc.
+        },
+      },
+    },
   },
   variants: {
     extend: {},
